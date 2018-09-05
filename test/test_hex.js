932e10195f68d98cb070691f7436a0ec6348d168
Fabio Caccamo <fabio.caccamo@gmail.com>
2018-09-05 15:51:23 +0200

    Added HexUtil tests.

--- test/test_hex.js
+++ test/test_hex.js
@@ -1,15 +1,114 @@
 var utils = require('../dist/utils.js');
 var test = utils.test;
+var hex = utils.hex;
 
 describe('hex', function() {
     describe('decodeInt', function() {
-        it('TODO', function() {
-            test.assertTrue(true);
+        var d = hex.decodeInt;
+        it('decode 0', function() {
+            test.assertEquals(d('00'), 0);
+        });
+        it('decode 1', function() {
+            test.assertEquals(d('01'), 1);
+        });
+        it('decode 2', function() {
+            test.assertEquals(d('02'), 2);
+        });
+        it('decode 3', function() {
+            test.assertEquals(d('03'), 3);
+        });
+        it('decode 4', function() {
+            test.assertEquals(d('04'), 4);
+        });
+        it('decode 5', function() {
+            test.assertEquals(d('05'), 5);
+        });
+        it('decode 6', function() {
+            test.assertEquals(d('06'), 6);
+        });
+        it('decode 7', function() {
+            test.assertEquals(d('07'), 7);
+        });
+        it('decode 8', function() {
+            test.assertEquals(d('08'), 8);
+        });
+        it('decode 9', function() {
+            test.assertEquals(d('09'), 9);
+        });
+        it('decode 10', function() {
+            test.assertEquals(d('0A'), 10);
+        });
+        it('decode 11', function() {
+            test.assertEquals(d('0B'), 11);
+        });
+        it('decode 12', function() {
+            test.assertEquals(d('0C'), 12);
+        });
+        it('decode 13', function() {
+            test.assertEquals(d('0D'), 13);
+        });
+        it('decode 14', function() {
+            test.assertEquals(d('0E'), 14);
+        });
+        it('decode 15', function() {
+            test.assertEquals(d('0F'), 15);
+        });
+        it('decode 16', function() {
+            test.assertEquals(d('10'), 16);
         });
     });
     describe('encodeInt', function() {
-        it('TODO', function() {
-            test.assertTrue(true);
+        var e = hex.encodeInt;
+        it('encode 0', function() {
+            test.assertEquals(e(0), '00');
+        });
+        it('encode 1', function() {
+            test.assertEquals(e(1), '01');
+        });
+        it('encode 2', function() {
+            test.assertEquals(e(2), '02');
+        });
+        it('encode 3', function() {
+            test.assertEquals(e(3), '03');
+        });
+        it('encode 4', function() {
+            test.assertEquals(e(4), '04');
+        });
+        it('encode 5', function() {
+            test.assertEquals(e(5), '05');
+        });
+        it('encode 6', function() {
+            test.assertEquals(e(6), '06');
+        });
+        it('encode 7', function() {
+            test.assertEquals(e(7), '07');
+        });
+        it('encode 8', function() {
+            test.assertEquals(e(8), '08');
+        });
+        it('encode 9', function() {
+            test.assertEquals(e(9), '09');
+        });
+        it('encode 10', function() {
+            test.assertEquals(e(10), '0A');
+        });
+        it('encode 11', function() {
+            test.assertEquals(e(11), '0B');
+        });
+        it('encode 12', function() {
+            test.assertEquals(e(12), '0C');
+        });
+        it('encode 13', function() {
+            test.assertEquals(e(13), '0D');
+        });
+        it('encode 14', function() {
+            test.assertEquals(e(14), '0E');
+        });
+        it('encode 15', function() {
+            test.assertEquals(e(15), '0F');
+        });
+        it('encode 16', function() {
+            test.assertEquals(e(16), '10');
         });
     });
 });
