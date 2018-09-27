1f9a65687e9852248bac771c6f0b23b239abbb70
Fabio Caccamo <fabio.caccamo@gmail.com>
2018-09-27 15:04:13 +0200

    Removed old comment.

--- src/utils/URLUtil.js
+++ src/utils/URLUtil.js
@@ -46,49 +46,6 @@ var URLUtil = {
 
     isLocalhost: function(url)
     {
-        // http://localhost
-        // http://localhost/
-        // http://localhost:8000
-        // http://localhost:8000/
-        // http://127.0.0.1
-        // http://127.0.0.1/
-        // http://127.0.0.1:8000
-        // http://127.0.0.1:8000/
-        // https://localhost
-        // https://localhost/
-        // https://localhost:8000
-        // https://localhost:8000/
-        // https://127.0.0.1
-        // https://127.0.0.1/
-        // https://127.0.0.1:8000
-        // https://127.0.0.1:8000/
-        // http://localhost
-        // http://localhost/
-        // http://localhost:8000
-        // http://localhost:8000/
-        // http://localhost:8000/
-        // http://127.0.0.1
-        // http://127.0.0.1/
-        // http://127.0.0.1:8000
-        // http://127.0.0.1:8000/
-        // https://localhost
-        // https://localhost/
-        // https://localhost:8000
-        // https://localhost:8000/
-        // https://localhosts
-        // https://localhosts/
-        // https://localhosts:8000
-        // https://localhosts:8000/
-        // https://localhosts:8000/index.html
-        // https://localhost/
-        // https://localhost:8000
-        // https://localhost:8000/
-        // https://localhost:8000/index.html
-        // https://127.0.0.1
-        // https://127.0.0.1/
-        // https://127.0.0.1:8000
-        // https://127.0.0.1:8000/index.html
-
         var re = /^(https?\:\/\/)(localhost|127\.0\.0\.1)(\:[\d]+)?(\/(.)*)?$/;
         return re.test((url || URLUtil.getURL()));
     }
