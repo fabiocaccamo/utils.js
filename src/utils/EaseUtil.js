ec5458c62c33b41769d4fdaaf1a1f4b649257cd8
Fabio Caccamo <fabio.caccamo@gmail.com>
2019-11-19 12:58:57 +0100

    Avoided assignments in operands in EaseUtil.

--- src/utils/EaseUtil.js
+++ src/utils/EaseUtil.js
@@ -11,10 +11,14 @@ var EaseUtil = {
     {
         // s = overshoot = 1.70158
         s = (isNaN(s) ? 1.70158 : s);
-        if ((t /= 0.5) < 1.0) {
-            return 0.5 * (t * t * (((s *= (1.525)) + 1.0) * t - s));
+        t /= 0.5;
+        if (t < 1.0) {
+            s *= 1.525;
+            return 0.5 * (t * t * ((s + 1.0) * t - s));
         } else {
-            return 0.5 * ((t -= 2.0) * t * (((s *= (1.525)) + 1.0) * t + s) + 2.0);
+            t -= 2.0;
+            s *= 1.525;
+            return 0.5 * (t * t * ((s + 1.0) * t + s) + 2.0);
         }
     },
 
@@ -22,7 +26,8 @@ var EaseUtil = {
     {
         // s = overshoot = 1.70158
         s = (isNaN(s) ? 1.70158 : s);
-        return (t -= 1.0) * t * (((s + 1.0) * t) + s) + 1.0;
+        t -= 1.0;
+        return t * t * (((s + 1.0) * t) + s) + 1.0;
     },
 
     bounceIn: function(t)
@@ -33,13 +38,16 @@ var EaseUtil = {
             return 1.0 - (7.5625 * t * t);
         }
         else if (t < (2.0 / 2.75)) {
-            return 1.0 - (7.5625 * (t -= (1.5 / 2.75)) * t + 0.75);
+            t -= (1.5 / 2.75);
+            return 1.0 - (7.5625 * t * t + 0.75);
         }
         else if (t < (2.5 / 2.75)) {
-            return 1.0 - (7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375);
+            t -= (2.25 / 2.75);
+            return 1.0 - (7.5625 * t * t + 0.9375);
         }
         else {
-            return 1.0 - (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375);
+            t -= (2.625 / 2.75);
+            return 1.0 - (7.5625 * t * t + 0.984375);
         }
     },
 
@@ -52,26 +60,32 @@ var EaseUtil = {
                 return 1.0 - (7.5625 * t * t);
             }
             else if (t < (2.0 / 2.75)) {
-                return 1.0 - (7.5625 * (t -= (1.5 / 2.75)) * t + 0.75);
+                t -= (1.5 / 2.75);
+                return 1.0 - (7.5625 * t * t + 0.75);
             }
             else if (t < (2.5 / 2.75)) {
-                return 1.0 - (7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375);
+                t -= (2.25 / 2.75);
+                return 1.0 - (7.5625 * t * t + 0.9375);
             }
             else {
-                return 1.0 - (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375);
+                t -= (2.625 / 2.75);
+                return 1.0 - (7.5625 * t * t + 0.984375);
             }
         } else {
             if (t < (1.0 / 2.75)) {
                 return (7.5625 * t * t);
             }
             else if (t < (2.0 / 2.75)) {
-                return (7.5625 * (t -= (1.5 / 2.75)) * t + 0.75);
+                t -= (1.5 / 2.75);
+                return (7.5625 * t * t + 0.75);
             }
             else if (t < (2.5 / 2.75)) {
-                return (7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375);
+                t -= (2.25 / 2.75);
+                return (7.5625 * t * t + 0.9375);
             }
             else {
-                return (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375);
+                t -= (2.625 / 2.75);
+                return (7.5625 * t * t + 0.984375);
             }
         }
     },
@@ -82,13 +96,16 @@ var EaseUtil = {
             return (7.5625 * t * t);
         }
         else if (t < (2.0 / 2.75)) {
-            return (7.5625 * (t -= (1.5 / 2.75)) * t + 0.75);
+            t -= (1.5 / 2.75);
+            return (7.5625 * t * t + 0.75);
         }
         else if (t < (2.5 / 2.75)) {
-            return (7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375);
+            t -= (2.25 / 2.75);
+            return (7.5625 * t * t + 0.9375);
         }
         else {
-            return (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375);
+            t -= (2.625 / 2.75);
+            return (7.5625 * t * t + 0.984375);
         }
     },
 
@@ -99,16 +116,18 @@ var EaseUtil = {
 
     circularInOut: function(t)
     {
-        if((t /= 0.5) < 1.0){
+        t /= 0.5;
+        if (t < 1.0) {
             return -0.5 * (Math.sqrt(1.0 - t * t) - 1.0);
         }
-
-        return 0.5 * (Math.sqrt(1.0 - (t -= 2.0) * t) + 1.0);
+        t -= 2.0;
+        return 0.5 * (Math.sqrt(1.0 - t * t) + 1.0);
     },
 
     circularOut: function(t)
     {
-        return Math.sqrt(1.0 - ((t -= 1.0) * t));
+        t -= 1.0;
+        return Math.sqrt(1.0 - (t * t));
     },
 
     cubicIn: function(t)
@@ -118,59 +137,55 @@ var EaseUtil = {
 
     cubicInOut: function(t)
     {
-        if ((t /= 0.5) < 1.0){
+        t /= 0.5;
+        if (t < 1.0) {
             return 0.5 * t * t * t;
         }
-
-        return 0.5 * ((t -= 2.0) * t * t + 2.0);
+        t -= 2.0;
+        return 0.5 * (t * t * t + 2.0);
     },
 
     cubicOut: function(t)
     {
-        return (t -= 1.0) * t * t + 1.0;
+        t -= 1.0;
+        return t * t * t + 1.0;
     },
 
     elasticIn: function(t, a, p)
     {
         // a = amplitude = 0.0, p = period = 0.3
-
         if (t === 0.0) {
             return 0.0;
         }
         if (t === 1.0) {
             return 1.0;
         }
-
         a = (isNaN(a) ? 0.0 : a);
         p = (isNaN(p) ? 0.3 : p);
-
         var s;
-
         if (a < 1.0) {
             a = 1.0;
             s = (p / 4.0);
         } else {
             s = (p / (2.0 * Math.PI) * Math.asin(1.0 / a));
         }
-        return -(a * Math.pow(2.0, 10.0 * (t -= 1.0)) * Math.sin((t - s) * (2.0 * Math.PI) / p));
+        t -= 1.0;
+        return -(a * Math.pow(2.0, 10.0 * t) * Math.sin((t - s) * (2.0 * Math.PI) / p));
     },
 
     elasticInOut: function(t, a, p)
     {
         // a = amplitude = 0.0, p = period = 0.3
-
         if (t === 0.0) {
             return 0.0;
         }
-        if ((t /= 0.5) === 2.0) {
+        t /= 0.5;
+        if (t === 2.0) {
             return 1.0;
         }
-
         a = (isNaN(a) ? 0.0 : a);
         p = (isNaN(p) ? 0.3 : p);
-
         var s;
-
         if (p === 0.3) {
             p *= 1.5;
         }
@@ -181,27 +196,25 @@ var EaseUtil = {
             s = (p / (2.0 * Math.PI) * Math.asin(1.0 / a));
         }
         if (t < 1.0) {
-            return -0.5 * (a * Math.pow(2.0, 10.0 * (t -= 1.0)) * Math.sin((t - s) * (2.0 * Math.PI) / p));
+            t -= 1.0;
+            return -0.5 * (a * Math.pow(2.0, 10.0 * t) * Math.sin((t - s) * (2.0 * Math.PI) / p));
         }
-        return a * Math.pow(2.0, -10.0 * (t -= 1.0)) * Math.sin((t - s) * (2.0 * Math.PI) / p) * 0.5 + 1.0;
+        t -= 1.0;
+        return a * Math.pow(2.0, -10.0 * t) * Math.sin((t - s) * (2.0 * Math.PI) / p) * 0.5 + 1.0;
     },
 
     elasticOut: function(t, a, p)
     {
         // a = amplitude = 0.0, p = period = 0.3
-
         if (t === 0.0) {
             return 0.0;
         }
         if (t === 1.0) {
             return 1.0;
         }
-
         a = (isNaN(a) ? 0.0 : a);
         p = (isNaN(p) ? 0.3 : p);
-
         var s;
-
         if (a < 1.0) {
             a = 1.0;
             s = (p / 4.0);
@@ -230,7 +243,8 @@ var EaseUtil = {
         if ((t /= 0.5) < 1.0) {
             return 0.5 * Math.pow(2.0, 10.0 * (t - 1.0));
         }
-        return 0.5 * (-Math.pow(2.0, -10.0 * (t -= 1.0)) + 2.0);
+        t -= 1.0;
+        return 0.5 * (-Math.pow(2.0, -10.0 * t) + 2.0);
     },
 
     exponentialOut: function(t)
@@ -256,7 +270,8 @@ var EaseUtil = {
         if ((t /= 0.5) < 1.0) {
             return 0.5 * t * t;
         }
-        return -0.5 * ((t -= 1.0) * (t - 2.0) - 1.0);
+        t -= 1.0;
+        return -0.5 * (t * (t - 2.0) - 1.0);
     },
 
     quadraticOut: function(t)
@@ -274,12 +289,14 @@ var EaseUtil = {
         if ((t /= 0.5) < 1.0) {
             return 0.5 * t * t * t * t;
         }
-        return -0.5 * ((t -= 2.0) * t * t * t - 2.0);
+        t -= 2.0;
+        return -0.5 * (t * t * t * t - 2.0);
     },
 
     quarticOut: function(t)
     {
-        return -((t -= 1.0) * t * t * t - 1.0);
+        t -= 1.0;
+        return -(t * t * t * t - 1.0);
     },
 
     quinticIn: function(t)
@@ -289,15 +306,18 @@ var EaseUtil = {
 
     quinticInOut: function(t)
     {
-        if ((t /= 0.5) < 1.0) {
+        t /= 0.5;
+        if (t < 1.0) {
             return 0.5 * t * t * t * t * t;
         }
-        return 0.5 * ((t -= 2.0) * t * t * t * t + 2.0);
+        t -= 2.0;
+        return 0.5 * (t * t * t * t * t + 2.0);
     },
 
     quinticOut: function(t)
     {
-        return (t -= 1.0) * t * t * t * t + 1.0;
+        t -= 1.0;
+        return t * t * t * t * t + 1.0;
     },
 
     sexticIn: function(t)
@@ -307,15 +327,18 @@ var EaseUtil = {
 
     sexticInOut: function(t)
     {
-        if ((t /= 0.5) < 1.0) {
+        t /= 0.5;
+        if (t < 1.0) {
             return 0.5 * t * t * t * t * t * t;
         }
-        return -0.5 * ((t -= 2.0) * t * t * t * t * t - 2.0);
+        t -= 2.0;
+        return -0.5 * (t * t * t * t * t * t - 2.0);
     },
 
     sexticOut: function(t)
     {
-        return -((t -= 1.0) * t * t * t * t * t - 1.0);
+        t -= 1.0;
+        return -(t * t * t * t * t * t - 1.0);
     },
 
     sineIn: function(t)
