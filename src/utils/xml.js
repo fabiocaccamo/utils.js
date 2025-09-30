67e85dffde3d7afae7e1a9bb8d3e2c40244dadb9
Fabio Caccamo <fabio.caccamo@gmail.com>
2025-10-01 00:28:01 +0200

    Fix polynomial regular expression used on uncontrolled data.

--- src/utils/xml.js
+++ src/utils/xml.js
@@ -23,7 +23,7 @@
 // };
 
 function removeNamespaces(str) {
-    return str.replace(/<[^>]*>/g, (tag) => {
+    return str.replace(/<[^>]*?>/g, (tag) => {
         return tag.replace(/(\s|<\/?)[a-zA-Z0-9]+\:/g, '$1');
     });
 }
